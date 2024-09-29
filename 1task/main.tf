735d2aa5cbca0c57618201f46d8ecfc0af03e7ec
begemkulova <egemkulova.bekaiym@gmail.com>
2024-09-30 02:26:34 +0600

    c1-21

--- 1task/main.tf
+++ 1task/main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = var.s3_bucket_name
-  acl    = "private"
-}
+# resource "aws_s3_bucket" "terraform_state_bucket" {
+#   bucket = var.s3_bucket_name
+#   acl    = "private"
+# }
 
-resource "aws_s3_bucket_versioning" "bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state_bucket.id
-  versioning_configuration {
-    status = var.bucket_versioning_status
-  }
-}
+# resource "aws_s3_bucket_versioning" "bucket_versioning" {
+#   bucket = aws_s3_bucket.terraform_state_bucket.id
+#   versioning_configuration {
+#     status = var.bucket_versioning_status
+#   }
+# }
 
