57fc902e59d963b8fe68af869e4b8680e3ab7545
begemkulova <egemkulova.bekaiym@gmail.com>
2024-10-14 04:11:29 +0600

    commit13

--- 2task/backend.tf
+++ 2task/backend.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = var.region
 }
-terraform {
-  backend "s3" {
-    bucket = "begemkulov-rs-terraform-state"
-    key    = "task2/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "begemkulov-rs-terraform-state"
+#     key    = "task2/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
