f6f3752ee0e7a6b8801b9be7b7641ddeb223cf9e
begemkulova <egemkulova.bekaiym@gmail.com>
2024-10-21 03:51:58 +0600

    task3-commit-2

--- 3task/bastion.sh
+++ 3task/bastion.sh
@@ -2,6 +2,6 @@
 sudo apt-get update -y
 sudo ufw disable
 mkdir -p /home/ubuntu/.ssh
-echo "${var.private_key}" > /home/ubuntu/.ssh/k8s-cluster.pem
-chmod 600 /home/ubuntu/.ssh/k8s-cluster.pem
-chown ubuntu:ubuntu /home/ubuntu/.ssh/k8s-cluster.pem
+echo "${var.private_key}" > /home/ubuntu/.ssh/key1234.pem
+chmod 600 /home/ubuntu/.ssh/key1234.pem
+chown ubuntu:ubuntu /home/ubuntu/.ssh/key1234.pem
